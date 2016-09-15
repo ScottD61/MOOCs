be09fa22fc2709293e4907de94a744402673afd4
Scott Davis <ScDavis6@gmail.com>
2016-09-15 13:26:18 -0700

    Update

---
+++ Udemy- SQL Bootcamp/Where.sql
@@ -0,0 +1,13 @@
+#Where challenge
+#1
+SELECT email FROM customer 
+WHERE first_name = 'Nancy' 
+AND last_name = 'Thomas';
+
+#2
+SELECT description FROM film 
+WHERE title = 'Outlaw Hanky';
+
+#3
+SELECT phone FROM address 
+WHERE address = '259 Ipoh Drive';
