5a687aad25e3b958a9875ee0459b68ca3e137cff
Scott Davis <scdavis6@gmail.com>
2023-11-09 10:31:35 -0800

    Delete Udemy-SQL Server directory

--- Udemy-SQL Server/Assignment1 - Create database.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE tblPrimeNumbers (intField int)
-INSERT INTO tblPrimeNumbers VALUES (2),(3),(5),(7),(11)
-SELECT * FROM tblPrimeNumbers
-TRUNCATE TABLE tblPrimeNumbers
-DROP TABLE tblPrimeNumberss
--- Udemy-SQL Server/SQL_Math.sql
+++
@@ -1,4 +0,0 @@
-SELECT 4+9 AS MyAnswer;
-SELECT 15-26 AS Balance;
-SELECT (24*4)+3 AS MyResponse;
-SELECT 48/4 AS Result;
